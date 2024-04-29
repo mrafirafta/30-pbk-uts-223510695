6f5cd2375fdcc7e1f023cd53aba594e10a3d2d87
halil fahriza <halilfahriza03@gmail.com>
2024-04-29 13:07:35 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<!-- src/App.vue -->
+<template>
+  <div id="app" :style="{ backgroundColor: '#fff' }">
+    <ActivityList />
+  </div>
+
+</template>
+
+<script>
+import ActivityList from './components/ActivityList.vue';
+
+export default {
+  components: {
+    ActivityList
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Arial, sans-serif;
+  margin: 0 auto;
+  padding: 20px;
+  max-width: 600px;
+}
+</style>
